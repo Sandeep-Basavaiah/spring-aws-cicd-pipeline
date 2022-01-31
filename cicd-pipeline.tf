06a97acb7695d7bf972030d5516568151fb8af64
Sandeep Basavaiah <sandeep.basavaiah82@gmail.com>
2022-01-31 12:43:20 -0500

    adding docker build stage

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -1,3 +1,28 @@
+resource "aws_codebuild_project" "build-docker" {
+  name          = "build-docker"
+  description   = "Build & push docker image to docker hub"
+  service_role  = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:latest"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    # registry_credential{
+    #     credential = var.dockerhub_credentials
+    #     credential_provider = "SECRETS_MANAGER"
+    # }
+ }
+ source {
+     type   = "CODEPIPELINE"
+     buildspec = file("buildspec/docker-buildspec.yml")
+ }
+}
+
 resource "aws_codebuild_project" "tf-plan" {
   name          = "tf-cicd-plan"
   description   = "Plan stage for terraform"
